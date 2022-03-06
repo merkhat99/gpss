835f44b591fd0c10852c86a9a042962efe459ae8
merkhat99 <cartman92_@mail.ru>
2022-03-06 16:43:42 +0600

    ipaddr

--- app.py
+++ app.py
@@ -17,5 +17,23 @@ def get_my_ip():
     return jsonify({'ip': request.remote_addr})
 
 
+@app.route('/main')
+def hello_world():
+    ip_addr = request.remote_addr
+    return '<h1> Your IP address is:' + ip_addr
+
+
+@app.route('/client')
+def client():
+    ip_addr = request.environ['REMOTE_ADDR']
+    return '<h1> Your IP address is:' + ip_addr
+
+
+@app.route('/clients')
+def proxy_client():
+    ip_addr = request.environ.get('HTTP_X_FORWARDED_FOR', request.remote_addr)
+    return '<h1> Your IP address is:' + ip_addr
+
+
 if __name__ == '__main__':
     app.run()
